81bf749b48bf721e813715aa668d2badc4724892
soniaperezcaceres <sonia.perez.caceres@hotmail.com>
2017-12-09 12:51:58 +0100

    contadorMovimiento.py
    Esta función se encarga de auditar si hay o no movimiento dentro X región. Dependiendo del paso en que se encuentre el usuario dentro de la secuencia, la región cambiará. 
    
    MODO DE USO: 
    - Correr mainCode.py, desde allí automáticamente correrá contadorMovimiento.py
    - Cuando en la imagen aparezca una figura del color del rango seleccionado en el paso anterior, el código lo encerrará en un rectángulo verde y lo seguirá. Si este objeto entra en la región (demarcada en color fucsia) y se queda allí dentro 'n' segundos ('n' es modificable dentro del código, manualmente), el código arrojará un mensaje satisfactorio y cerrará la ventana. De lo contrario arrojará un mensaje de error hasta que cumpla el tiempo.

---
+++ contadorMovimiento.py
@@ -0,0 +1,132 @@
+    # -*- coding: utf-8 -*-
+def funcionMovimientoEnRegion(regionCoordinates,tiempo,noRegion,lowerValue,upperValue):
+    
+    import cv2
+    import numpy as np
+    import time
+    from nms_fast import mincuadro
+     
+    cap = cv2.VideoCapture(0)
+    m=25
+    m1=500
+    chrono = 0
+    
+    def colorChange(frame):
+    #convertimos a escalas de grises
+        H = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) #Convertirlo a espacio de color HSV
+        return H
+    
+    def overlapRectangles(reg,rect):
+          if(reg[2]<rect[0] or rect[2]<reg[0] or reg[3]<rect[1] or rect[3]<reg[1]):
+              return False
+          else:
+              return True
+          
+    def currentTime(): # Obtain current time in seconds
+        time1 = time.localtime()
+        time2 = (time1[0],time1[1],time1[2],time1[3],time1[4],time1[5],time1[6],time1[7],time1[8])
+        currentTime = time.mktime(time2)
+        return currentTime
+    
+    def colorFilter(frame):
+    #Se crea un array con las posiciones minimas y maximas
+        lower=lowerValue
+        upper=upperValue
+    #Deteccion de colores
+        mask = cv2.inRange(frame, lower, upper)
+        return mask
+    
+    def nothing(x):
+       pass
+    
+    #nos servira para obterner el fondo
+    fondo = None
+    
+    #recorremos todos los frames
+    while(1):
+      _,frame = cap.read() #Leer un frame
+      #print(type(frame))
+      frame2 = np.copy (frame)
+      gris = colorChange(frame)
+      gris = colorFilter(gris)
+      
+    #aplicamos suavizado para eliminar el ruido
+      gris = cv2.GaussianBlur(gris, (21, 21), 0)
+      
+    #si todavia no hemos obtenido el fonod, lo obtenemos
+      if fondo is None:
+          fondo = gris
+          continue
+      
+    #calculo la dif etre el fondo y el frame
+      resta = cv2.absdiff(fondo, gris)
+     
+    # Aplicamos un umbral
+      umbral = cv2.threshold(resta, m, 255, cv2.THRESH_BINARY)[1]
+     
+    # Dilatamos el umbral para tapar agujeros
+      umbral = cv2.dilate(umbral, None, iterations=2)
+     
+    # Copiamos el umbral para detectar los contornos
+      contornosimg = umbral.copy()
+      
+    # Buscamos contorno en la imagen
+      contornos, hierarchy = cv2.findContours(contornosimg,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+      cajas = []
+    # Recorremos todos los contornos encontrados
+      for c in contornos:
+    # Eliminamos los contornos mas pequenos
+           if cv2.contourArea(c) < m1:
+              continue    
+    # Obtenemos el bounds del contorno, el rectangulo mayor que engloba al contorno
+           (x, y, w, h) = cv2.boundingRect(c)
+    # Dibujamos el rectangulo del bounds
+           cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+           cajas.append((x,y,x+w,y+h))
+      
+      image2 = mincuadro(frame2,np.array(cajas)) 
+      
+      # Define si hay movimiento dentro de las regiones FIXED FOR 3 REGIONS
+            
+      for rect in cajas:
+          overlap = overlapRectangles(regionCoordinates,rect)
+          if overlap:
+              if chrono == 0:
+                  chrono = currentTime()
+              else:
+                  chrono2 = currentTime()
+                  if (chrono2 - chrono) >= tiempo:
+                      cap.release()
+                      cv2.destroyAllWindows()
+                      return True
+                      break
+                  print "Hay movimiento en la region ", noRegion
+          else:
+              print('Se saltó un paso, recomience')
+              chrono = 0
+                  
+                  
+             
+      cv2.rectangle(image2, (regionCoordinates[0], regionCoordinates[1]), (regionCoordinates[2], regionCoordinates[3]), (255, 0, 255), 2) # Con las coordenadas construye el rectángulo
+   
+      
+      #Mostrar los resultados y salir
+      
+      #cv2.imshow('camara',frame)
+      #cv2.imshow('gris',gris)
+      #cv2.imshow("Umbral", umbral)
+      #cv2.imshow("Resta", resta)
+      #cv2.imshow("Contorno", contornosimg)
+      cv2.imshow("other", image2)
+
+      k = cv2.waitKey(5) & 0xFF
+  
+  #tiempo de espera para que se vea bien
+      time.sleep(0.015)
+    
+  #si ha pulsado escape para salir, salimos
+      if k == 27:
+    #liberamos la camara y cerramos todas las ventanas
+        cap.release()
+        cv2.destroyAllWindows()
+    
